dbff7219af1634874a10929a8e3e6d74c348d8ab
stakbucks <dhassidu11@gmail.com>
2023-02-23 21:38:16 +0900

    로그인 실패 메시지 추가

--- frontend/src/pages/account/Login.tsx
+++ frontend/src/pages/account/Login.tsx
@@ -15,6 +15,7 @@ import { useCookies } from "react-cookie";
 import { useRecoilState } from "recoil";
 import { IUserLoggedIn } from "../../interfaces/User";
 import { userLoggedIn } from "../../recoil/atoms/userAtom";
+import { useState } from "react";
 const Wrapper = styled.div`
   width: 50vw;
   display: flex;
@@ -26,8 +27,13 @@ const Form = styled.form`
   flex-direction: column;
   gap: 10px;
 `;
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 10px;
+`;
 const GithubLogin = styled.div``;
 function Login() {
+  const [error, setError] = useState(false);
   const [loggedIn, setLoggedIn] = useRecoilState<IUserLoggedIn>(userLoggedIn);
   const navigate = useNavigate();
   const { register, handleSubmit, watch } = useForm<IUser>();
@@ -46,6 +52,7 @@ function Login() {
       navigate(-1);
     } catch (error) {
       console.log(error);
+      setError(true);
     }
   };
 
@@ -64,8 +71,12 @@ function Login() {
             type="password"
           />
         </div>
+        {error ? (
+          <ErrorMessage>아이디 또는 비밀번호가 일치하지 않습니다.</ErrorMessage>
+        ) : null}
         <button>로그인</button>
       </Form>
+
       {/* <GithubLogin onClick={getGithub}>깃허브로 로그인하기</GithubLogin> */}
     </Wrapper>
   );
